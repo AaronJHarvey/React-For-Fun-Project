039327fb19aa7627bf4d40846011372c9645e78e
Aaron <Harvey.AaronJ@gmail.com>
2022-10-17 07:25:20 -0400

    Builds out main section content

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
 export default function Main () {
-	return <h2>Main goes here</h2>;
+	return (
+		<div className='main-section'>
+			<h1 className='main-title'>Fun facts about React</h1>
+			<ul className='react-facts-list'>
+				<li className='item-1'>Was first released in 2013</li>
+				<li className='item-2'>Was Originally Createdd by Jordan Walke</li>
+				<li className='item-3'>Has well over 100k stars on Github</li>
+				<li className='item-4'>Is Maintained by Facebook</li>
+				<li className='item-5'>Powers thousands of enterprise apps, including mobile apps</li>
+			</ul>
+		</div>
+	);
 }
